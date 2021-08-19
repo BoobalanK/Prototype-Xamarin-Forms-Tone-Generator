20eac8db914e57decda825ae5fa30b3fa89edfe2
Boobalan Kothandaraman <kboobalan93@gmail.com>
2021-08-19 19:20:56 +0530

    Add project files.

---
+++ App1/App1.Android/ToneService.cs
@@ -0,0 +1,64 @@
+﻿
+using Android.Media;
+
+using App1.Droid;
+
+using Java.Lang;
+
+using System;
+using System.Linq;
+
+using Xamarin.Forms;
+
+using static Android.Renderscripts.Sampler;
+
+[assembly: Dependency(typeof(ToneService))]
+namespace App1.Droid
+{
+    public class ToneService : IToneService
+    {
+        private int volume = 0;
+        private ToneGenerator toneGenerator;
+        public ToneService()
+        {
+            int percantageVolume = 100;
+            if (volume == 0)
+            {
+                percantageVolume = 100;
+            }
+            if (volume == 1)
+            {
+                percantageVolume = 75;
+            }
+            if (volume == 2)
+            {
+                percantageVolume = 50;
+            }
+            if (volume == 3)
+            {
+                percantageVolume = 0;
+            }
+
+            toneGenerator = new ToneGenerator(Stream.Dtmf, percantageVolume);
+        }
+        public string[] GetToneTypes()
+        {
+            return System.Enum.GetValues(typeof(Tone)).Cast<Tone>().Select(x => x.ToString()).ToArray();
+        }
+        public void StartTone(int durationInMs)
+        {
+            toneGenerator.StartTone(Tone.PropBeep);
+        }
+
+        public void StartTone(string toneType)
+        {
+            Tone toneTypeEnum = (Tone)System.Enum.Parse(typeof(Tone), toneType);
+            toneGenerator.StartTone(toneTypeEnum);
+        }
+
+        public void StopTone()
+        {
+            toneGenerator.StopTone();
+        }
+    }
+}
---
+++ App1/App1/IToneService.cs
@@ -0,0 +1,10 @@
+﻿namespace App1
+{
+    public interface IToneService
+    {
+        string[] GetToneTypes();
+        void StartTone(string toneType);
+        void StartTone(int durationInMs);
+        void StopTone();
+    }
+}
